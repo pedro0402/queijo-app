16fbeef88985a90f4d1cb37ec69eab62917d2f8e
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-16 12:59:32 -0300

    [feat]: criação da classe Sale

---
+++ src/main/java/com/example/queijo_app/model/Sale.java
@@ -0,0 +1,34 @@
+package com.example.queijo_app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "costumer_id")
+    private Costumer costumer;
+
+    @NotNull
+    @Column(name = "sale_date")
+    private LocalDate saleDate;
+
+    @NotNull
+    @Column(name = "total_value")
+    private BigDecimal totalValue;
+
+    @NotNull
+    @Column(name = "total_cost")
+    private BigDecimal totalCost;
+
+    @NotNull
+    private BigDecimal profit;
+}

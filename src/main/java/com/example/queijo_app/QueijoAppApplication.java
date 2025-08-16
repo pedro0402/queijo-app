702ff56dc68572b1c45735fae91e32363bf4a367
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-16 12:01:58 -0300

    iniciando projeto

---
+++ src/main/java/com/example/queijo_app/QueijoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.queijo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueijoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueijoAppApplication.class, args);
+	}
+
+}

05dd105d25b77c25d59a4eeb30f7cd762eec0df2
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 16:56:16 -0300

    [feat]: criando instância do CustomerService no CustomerController

--- src/main/java/com/example/queijo_app/controller/CustomerController.java
+++ src/main/java/com/example/queijo_app/controller/CustomerController.java
@@ -1,10 +1,16 @@
 package com.example.queijo_app.controller;
 
+import com.example.queijo_app.service.CustomerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customer")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+
     
 }

78e4014f165cf05a8e4fd7f5579179ef91669208
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 10:38:32 -0300

    [refactor] alterando nome da classe de Costumer -> Customer

---
+++ src/main/java/com/example/queijo_app/model/Customer.java
@@ -0,0 +1,19 @@
+package com.example.queijo_app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String address;
+}

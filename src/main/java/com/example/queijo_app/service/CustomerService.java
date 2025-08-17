9e410ccf3fe5f3fb926c5df092b07dfdc775d3b7
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 12:03:57 -0300

    [feat]: adicionando método de save ao CustomerService

--- src/main/java/com/example/queijo_app/service/CustomerService.java
+++ src/main/java/com/example/queijo_app/service/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.queijo_app.service;
 
+import com.example.queijo_app.model.Customer;
 import com.example.queijo_app.repository.CustomerRepository;
+import com.example.queijo_app.validator.CustomerValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,9 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
     private final CustomerValidator customerValidator;
 
-    
+    public Customer save(Customer customer) {
+        customerValidator.validate(customer);
+        return customerRepository.save(customer);
+    }
 
 }

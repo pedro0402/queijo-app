025c66a6e0c6828debd716feb2333a65d236de9a
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 11:47:40 -0300

    [feat]: adicionando métodos existsByNameAndAddress e existsByNameAndAddressAndIdNot no CustomerRepository

--- src/main/java/com/example/queijo_app/repository/CustomerRepository.java
+++ src/main/java/com/example/queijo_app/repository/CustomerRepository.java
@@ -3,5 +3,10 @@ package com.example.queijo_app.repository;
 import com.example.queijo_app.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    boolean existsByNameAndAddress(String name, String address);
+
+    boolean existsByNameAndAddressAndIdNot(String name, String address, Long id);
 }

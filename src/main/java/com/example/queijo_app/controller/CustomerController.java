65a43297a0f622739c0be9c9b9d3988accfa2e2a
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 17:39:44 -0300

    [feat]: criação do método save no CustomerController

--- src/main/java/com/example/queijo_app/controller/CustomerController.java
+++ src/main/java/com/example/queijo_app/controller/CustomerController.java
@@ -1,16 +1,31 @@
 package com.example.queijo_app.controller;
 
+import com.example.queijo_app.dto.CustomerDTO;
+import com.example.queijo_app.mapper.CustomerMapper;
+import com.example.queijo_app.model.Customer;
 import com.example.queijo_app.service.CustomerService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/customer")
 @RequiredArgsConstructor
-public class CustomerController {
+public class CustomerController implements GenericController {
 
     private final CustomerService customerService;
+    private final CustomerMapper customerMapper;
 
-    
+    public ResponseEntity<Void> save(@RequestBody @Valid CustomerDTO customerDTO, HttpServletRequest request) {
+        Customer customer = customerMapper.toEntity(customerDTO);
+        customerService.save(customer);
+        URI uri = headerLocation(customer.getId());
+        return ResponseEntity.created(uri).build();
+    }
 }

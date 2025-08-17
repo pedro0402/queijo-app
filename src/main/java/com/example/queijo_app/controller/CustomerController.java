e7b3cc866fcf67442b21e74a14708614ce5b16f1
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 17:59:36 -0300

    [feat]: adicionando notação @PostMapping no método save

--- src/main/java/com/example/queijo_app/controller/CustomerController.java
+++ src/main/java/com/example/queijo_app/controller/CustomerController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/customer")
 @RequiredArgsConstructor
+@RequestMapping("/customer")
 public class CustomerController implements GenericController {
 
     private final CustomerService customerService;
     private final CustomerMapper customerMapper;
 
+    @PostMapping
     public ResponseEntity<Void> save(@RequestBody @Valid CustomerDTO customerDTO, HttpServletRequest request) {
         Customer customer = customerMapper.toEntity(customerDTO);
         customerService.save(customer);

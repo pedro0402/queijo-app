705cf9828107ed7783026eabc85c474d337f96b6
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 11:46:13 -0300

    [refactor]: adicionando campos de Getter, Setter e ToString na classe Customer

--- src/main/java/com/example/queijo_app/model/Customer.java
+++ src/main/java/com/example/queijo_app/model/Customer.java
@@ -2,8 +2,14 @@ package com.example.queijo_app.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Customer {
 
     @Id

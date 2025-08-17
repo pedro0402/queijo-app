9a5d22979e65b02945fb339546d77bc2abb9845f
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-17 11:54:04 -0300

    [feat]: adicionando final no customerRepository e adicionando uma instância do CustomerValidator na classe CustomerService

--- src/main/java/com/example/queijo_app/service/CustomerService.java
+++ src/main/java/com/example/queijo_app/service/CustomerService.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerService {
 
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
+    private final CustomerValidator customerValidator;
 
     
 

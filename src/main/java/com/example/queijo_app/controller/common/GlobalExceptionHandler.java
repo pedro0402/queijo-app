f2b6601780b2d810fa1c6079e4251e9f883e7e6f
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-20 17:25:13 -0300

    [feat]: criação do método handleDuplicateCustomerException para tratamento de exceção

--- src/main/java/com/example/queijo_app/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/queijo_app/controller/common/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.queijo_app.controller.common;
 
 import com.example.queijo_app.dto.ErrorResponse;
 import com.example.queijo_app.exception.CustomerNotFoundException;
+import com.example.queijo_app.exception.DuplicateCustomerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +15,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleCustomerNotFoundException(CustomerNotFoundException e) {
         return ErrorResponse.notFound(e.getMessage());
+    }
+
+    @ExceptionHandler(DuplicateCustomerException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleDuplicateCustomerException(DuplicateCustomerException e) {
+        return ErrorResponse.conflict(e.getMessage());
     }
 
 }

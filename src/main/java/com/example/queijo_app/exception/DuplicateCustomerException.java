7a7a280b08be2a944142db9b100e3076a1c32778
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-20 16:31:09 -0300

    [feat]: adicionando a anotação @ResponseStatus para tratamento de exceção

--- src/main/java/com/example/queijo_app/exception/DuplicateCustomerException.java
+++ src/main/java/com/example/queijo_app/exception/DuplicateCustomerException.java
@@ -1,5 +1,9 @@
 package com.example.queijo_app.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateCustomerException extends RuntimeException {
     public DuplicateCustomerException(String message) {
         super(message);

8c603777c3a0a676da7c6b961d3797c56ac66189
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-20 14:37:13 -0300

    [feat]: criação do método customerToCustomersDTOs na interface CustomerMapper

--- src/main/java/com/example/queijo_app/mapper/CustomerMapper.java
+++ src/main/java/com/example/queijo_app/mapper/CustomerMapper.java
@@ -4,9 +4,16 @@ import com.example.queijo_app.dto.CustomerDTO;
 import com.example.queijo_app.model.Customer;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
     Customer toEntity(CustomerDTO customerDTO);
+
+
+    List<CustomerDTO> customerToCustomersDTOs(List<Customer> customerStream);
+
+
     CustomerDTO toCustomerDTO(Customer customer);
 }

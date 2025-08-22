13988f02cb27e40834136f749b723ccc2e8a0228
Pedro Moraes <pedro693.ph@gmail.com>
2025-08-21 22:56:21 -0300

    [feat]: criação do método handleMethodArgumentNotValidException para tratamento de exceções de forma personalizada

--- src/main/java/com/example/queijo_app/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/queijo_app/controller/common/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.example.queijo_app.controller.common;
 
+import com.example.queijo_app.dto.ErrorField;
 import com.example.queijo_app.dto.ErrorResponse;
 import com.example.queijo_app.exception.CustomerNotFoundException;
 import com.example.queijo_app.exception.DuplicateCustomerException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +26,16 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleDuplicateCustomerException(DuplicateCustomerException e) {
         return ErrorResponse.conflict(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        List<ErrorField> errorFields = fieldErrors.stream()
+                .map(fe -> new ErrorField(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+        return new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Validation failed for one or more fields", errorFields);
     }
 
 }
